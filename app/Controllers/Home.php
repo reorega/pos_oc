0f5e5729d71e0c4eab1e0c07ad7256e31cc50d7e
fradanaa <fradanafradana31@gmail.com>
2024-05-29 12:56:42 +0700

    update_setting2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\SettingModel;
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('login');
+        $settingModel = new SettingModel();
+        $data['setting'] = $settingModel->findAll();
+        return view('login',$data);
     }
 }
