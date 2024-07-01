0e62d38611a48548d2f2a562c6640413eebf4067
Fajar <fajarsatria4321@gmail.com>
2024-07-01 15:45:36 +0700

    retur

--- app/Views/admin/tablereturbarang.php
+++ app/Views/admin/tablereturbarang.php
@@ -1,16 +1,27 @@
-<table class="table table-hover mt-2 table-bordered">
-    <thead class="thead-dark">
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Produk</th>
-            <th scope="col">Supplier</th>
-            <th scope="col">Jumlah</th>
-            <th scope="col">Keterangan</th>
-            <th scope="col">Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($returbarang as $key => $rtb) : ?>
+<style>
+    .table-wrapper thead th {
+        background-color: #343a40;
+        color: #ffffff;
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
+</style>
+
+<div class="table-wrapper">
+    <table class="table table-hover mt-2 table-bordered">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Produk</th>
+                <th scope="col">Supplier</th>
+                <th scope="col">Jumlah</th>
+                <th scope="col">Keterangan</th>
+                <th scope="col">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($returbarang as $key => $rtb) : ?>
             <tr>
                 <td><?= $key + 1 ?></td>
                 <td><?= $rtb['product_name'] ?></td>
@@ -19,16 +30,19 @@
                 <td><?= $rtb['keterangan'] ?></td>
                 <td>
                     <!-- Button detail -->
-                    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#infoData<?= $rtb['id_retur_barang'] ?>">
+                    <button type="button" class="btn btn-info" data-toggle="modal"
+                        data-target="#infoData<?= $rtb['id_retur_barang'] ?>">
                         <i class="fa fa-eye"></i> Detail
                     </button>
 
                     <!-- Modal detail -->
-                    <div class="modal fade" id="infoData<?= $rtb['id_retur_barang'] ?>" tabindex="-1" aria-labelledby="infoDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
+                    <div class="modal fade" id="infoData<?= $rtb['id_retur_barang'] ?>" tabindex="-1"
+                        aria-labelledby="infoDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="infoDataLabel<?= $rtb['id_retur_barang'] ?>">Detail Data</h5>
+                                    <h5 class="modal-title" id="infoDataLabel<?= $rtb['id_retur_barang'] ?>">Detail Data
+                                    </h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -42,7 +56,7 @@
                                                 <th scope="col">Supplier</th>
                                                 <th scope="col">Jumlah</th>
                                                 <th scope="col">Keterangan</th>
-                                                
+
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -52,9 +66,9 @@
                                                 <td><?= $rtb['supplier_name'] ?></td>
                                                 <td><?= $rtb['jumlah'] ?></td>
                                                 <td><?= $rtb['keterangan'] ?></td>
-                                             
-                                                    <!-- Kosong karena ini hanya modal detail -->
-                                            
+
+                                                <!-- Kosong karena ini hanya modal detail -->
+
                                             </tr>
                                         </tbody>
                                     </table>
@@ -65,48 +79,66 @@
                     <!-- End Modal detail -->
 
                     <!-- Button edit -->
-                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editData<?= $rtb['id_retur_barang'] ?>">
+                    <button type="button" class="btn btn-warning" data-toggle="modal"
+                        data-target="#editData<?= $rtb['id_retur_barang'] ?>">
                         <i class="fa fa-pencil"></i> Edit
                     </button>
 
                     <!-- Modal edit -->
-                    <div class="modal fade" id="editData<?= $rtb['id_retur_barang'] ?>" tabindex="-1" aria-labelledby="editDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
+                    <div class="modal fade" id="editData<?= $rtb['id_retur_barang'] ?>" tabindex="-1"
+                        aria-labelledby="editDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="editDataLabel<?= $rtb['id_retur_barang'] ?>">Edit Data</h5>
+                                    <h5 class="modal-title" id="editDataLabel<?= $rtb['id_retur_barang'] ?>">Edit Data
+                                    </h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </div>
                                 <div class="modal-body">
                                     <form action="" method="post" enctype="multipart/form-data">
-                                        <input type="hidden" name="id_retur_barang" value="<?= $rtb['id_retur_barang'] ?>">
+                                        <input type="hidden" name="id_retur_barang"
+                                            value="<?= $rtb['id_retur_barang'] ?>">
                                         <div class="form-group">
-                                            <label for="editProduk<?= $rtb['id_retur_barang'] ?>" class="form-label">Nama Produk</label>
-                                            <select class="form-control selectpicker" aria-label="Default select example" id="editProduk<?= $rtb['id_retur_barang'] ?>" data-live-search="true" disabled>
+                                            <label for="editProduk<?= $rtb['id_retur_barang'] ?>"
+                                                class="form-label">Nama Produk</label>
+                                            <select class="form-control selectpicker"
+                                                aria-label="Default select example"
+                                                id="editProduk<?= $rtb['id_retur_barang'] ?>" data-live-search="true"
+                                                disabled>
                                                 <option selected disabled>Pilih Produk</option>
                                                 <?php foreach ($produk as $product) : ?>
-                                                    <option value="<?= $product['id_produk']; ?>" <?= ($rtb['produk_id'] == $product['id_produk']) ? 'selected' : ''; ?>>
-                                                        <?= $product['nama_produk'] ?>
-                                                    </option>
+                                                <option value="<?= $product['id_produk']; ?>"
+                                                    <?= ($rtb['produk_id'] == $product['id_produk']) ? 'selected' : ''; ?>>
+                                                    <?= $product['nama_produk'] ?>
+                                                </option>
                                                 <?php endforeach; ?>
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <label for="editJumlah<?= $rtb['id_retur_barang'] ?>" class="form-label">Jumlah Retur</label>
-                                            <input type="text" class="form-control" id="editJumlah<?= $rtb['id_retur_barang'] ?>" name="jumlah" value="<?= $rtb['jumlah'] ?>">
+                                            <label for="editJumlah<?= $rtb['id_retur_barang'] ?>"
+                                                class="form-label">Jumlah Retur</label>
+                                            <input type="text" class="form-control"
+                                                id="editJumlah<?= $rtb['id_retur_barang'] ?>" name="jumlah"
+                                                value="<?= $rtb['jumlah'] ?>">
                                         </div>
                                         <div class="form-group">
-                                            <label for="editKeterangan<?= $rtb['id_retur_barang'] ?>" class="form-label">Keterangan</label>
-                                            <input type="text" class="form-control" id="editKeterangan<?= $rtb['id_retur_barang'] ?>" name="keterangan" value="<?= $rtb['keterangan'] ?>">
+                                            <label for="editKeterangan<?= $rtb['id_retur_barang'] ?>"
+                                                class="form-label">Keterangan</label>
+                                            <input type="text" class="form-control"
+                                                id="editKeterangan<?= $rtb['id_retur_barang'] ?>" name="keterangan"
+                                                value="<?= $rtb['keterangan'] ?>">
                                         </div>
                                         <!-- Hidden input for the current date -->
                                         <input type="hidden" name="tanggal" value="<?= date('Y-m-d') ?>">
                                         <!-- End of hidden input -->
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                                            <button type="button" class="btn btn-success" onclick="editData('<?= $rtb['id_retur_barang'] ?>')">Simpan Perubahan</button>
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Batal</button>
+                                            <button type="button" class="btn btn-success"
+                                                onclick="editData('<?= $rtb['id_retur_barang'] ?>')">Simpan
+                                                Perubahan</button>
                                         </div>
                                     </form>
                                 </div>
@@ -116,16 +148,19 @@
                     <!-- End Modal edit -->
 
                     <!-- Button hapus -->
-                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapusData<?= $rtb['id_retur_barang'] ?>">
+                    <button type="button" class="btn btn-danger" data-toggle="modal"
+                        data-target="#hapusData<?= $rtb['id_retur_barang'] ?>">
                         <i class="fa fa-trash"></i> Hapus
                     </button>
 
                     <!-- Modal hapus -->
-                    <div class="modal fade" id="hapusData<?= $rtb['id_retur_barang'] ?>" tabindex="-1" aria-labelledby="hapusDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
+                    <div class="modal fade" id="hapusData<?= $rtb['id_retur_barang'] ?>" tabindex="-1"
+                        aria-labelledby="hapusDataLabel<?= $rtb['id_retur_barang'] ?>" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="hapusDataLabel<?= $rtb['id_retur_barang'] ?>">Konfirmasi Hapus</h5>
+                                    <h5 class="modal-title" id="hapusDataLabel<?= $rtb['id_retur_barang'] ?>">Konfirmasi
+                                        Hapus</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -135,8 +170,10 @@
                                 </div>
                                 <div class="modal-footer">
                                     <form id="formHapus<?= $rtb['id_retur_barang'] ?>" action="" method="">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                                        <button type="button" class="btn btn-danger" onclick="hapusData('<?= $rtb['id_retur_barang'] ?>')">Hapus Data</button>
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Batal</button>
+                                        <button type="button" class="btn btn-danger"
+                                            onclick="hapusData('<?= $rtb['id_retur_barang'] ?>')">Hapus Data</button>
                                     </form>
                                 </div>
                             </div>
@@ -145,10 +182,11 @@
                     <!-- End Modal hapus -->
                 </td>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 
 <?php if ($search == "no") : ?>
-    <?= $pager->links() ?>
+<?= $pager->links() ?>
 <?php endif; ?>
