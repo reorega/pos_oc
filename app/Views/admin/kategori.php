1cc10b99b29147935196619a079999f418adf009
krisna <kvanzalu@gmail.com>
2024-07-29 16:06:44 +0700

    vlds

--- app/Views/admin/kategori.php
+++ app/Views/admin/kategori.php
@@ -2,9 +2,7 @@
 <?= $this->section('content') ?>
 <div class="content-wrapper">
     <section class="content-header">
-        <h1>
-            Tabel Kategori
-        </h1>
+        <h1>Tabel Kategori</h1>
     </section>
     <input type="hidden" id="page" value="1">
     <section class="content">
@@ -12,12 +10,12 @@
             <div class="box-body">
                 <div class="row">
                     <div class="col-md-9">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#tambahData">
+                        <button type="button" class="btn btn-primary" onclick="bukaModalTambah()">
                             <i class="fa fa-plus-square"></i> Tambah Data
                         </button>
                     </div>
                     <div class="col-md-3">
-                        <div class="input-group pull-right">
+                        <div class="input-group">
                             <span class="input-group-addon bg-primary"><i class="fa fa-search"></i></span>
                             <input type="text" class="form-control" id="search" name="search" placeholder="Pencarian">
                         </div>
@@ -29,6 +27,7 @@
         </div>
     </section>
 </div>
+
 <!-- Modal Tambah Data -->
 <div class="modal fade" id="tambahData" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -43,31 +42,65 @@
                 <form id="formTambahKategori" action="<?= site_url('/admin/tambahDataKategori') ?>" method="post">
                     <?= csrf_field() ?>
                     <div class="form-group">
-                        <label for="inputNamaKategori" class="form-label">Nama Kategori</label>
-                        <input type="text" class="form-control" id="inputNamaKategori" name="nama_kategori" required>
+                        <label for="inputNamaKategori" class="control-label">Nama Kategori</label>
+                        <input type="text" class="form-control" id="inputNamaKategori" name="nama_kategori">
+                        <p class="invalid-feedback text-danger"></p>
                     </div>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                    <button type="button" class="btn btn-success" onclick="tambahData()">Simpan Data</button>
+                    <button type="submit" class="btn btn-success">Simpan Data</button>
                 </form>
             </div>
         </div>
     </div>
 </div>
+
 <script src="<?= base_url('assets/js/jquery-3.7.1.min.js');?>"></script>
+<script src="<?= base_url('assets/js/sweetalert2.js'); ?>"></script>
 <script>
     $(document).ready(function() {
         var page = $('#page').val();
         ambilData();
+
         $('#search').keyup(function() {
             console.log($('#search').val())
-            ambilData();
+        ambilData();
         });
         $(document).on('click', '.pagination a', function(event) {
             event.preventDefault();
             page = $(this).attr('href').split('page=')[1];
             ambilData(page);
         });
-        console.log(page);
+
+        $('#formTambahKategori').on('submit', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                type: 'POST',
+                data: $(this).serialize(),
+                dataType: 'json',
+                success: function (response) {
+                    $('.invalid-feedback').empty();
+                    $('.form-group').removeClass('has-error').removeClass('has-feedback');
+                    $('.form-group .glyphicon').remove();
+
+                    if (response.success) {
+                        $('#tambahData').modal('hide');
+                        ambilData($('#page').val());
+                        Swal.fire('Tersimpan!', 'Data berhasil ditambahkan.', 'success');
+                    } else {
+                        $.each(response.errors, function (field, message) {
+                            var element = $('[name=' + field + ']');
+                            element.closest('.form-group').addClass('has-error has-feedback');
+                            element.next('.invalid-feedback').text(message);
+                            element.after('<span class="glyphicon glyphicon-warning-sign form-control-feedback text-danger"></span>');
+                        });
+                    }
+                },
+                error: function (xhr, thrownError) {
+                    alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                }
+            });
+        });
     });
 
     function ambilData(page = 1) {
@@ -110,18 +143,18 @@
     }
 
     function editData(id_kategori) {
-        idkat = id_kategori
+        idkat=id_kategori
         $.ajax({
             type: "post",
             url: "<?= site_url('/admin/editDataKategori') ?>",
             data: {
-                id: idkat,
-                edit_nama_kategori: $('#editNamaKategori' + idkat).val(),
+                id: id_kategori,
+                nama_kategori: $('#editNamaKategori' + id_kategori).val(),
             },
             dataType: "json",
             success: function(response) {
                 ambilData($('#page').val());
-                $('#editData' + idkat).modal('hide');
+                $('#editData' + id_kategori).modal('hide');
             },
             error: function(xhr, thrownError) {
                 alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
@@ -130,22 +163,47 @@
     }
 
     function hapusData(id_kategori) {
-        idkat = id_kategori
-        $.ajax({
-            type: "post",
-            url: "<?= site_url('/admin/hapusDataKategori') ?>",
-            data: {
-                id: idkat,
-            },
-            dataType: "json",
-            success: function(response) {
-                ambilData($('#page').val());
-                $('#hapusData' + idkat).modal('hide');
-            },
-            error: function(xhr, thrownError) {
-                alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+        idkat=id_kategori
+        Swal.fire({
+            title: 'Apakah kamu yakin?',
+            text: "Kamu tidak dapat mengembalikan datanya.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, Hapus data!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "post",
+                    url: "<?= site_url('/admin/hapusDataKategori') ?>",
+                    data: {
+                        id: id_kategori,
+                    },
+                    dataType: "json",
+                    success: function(response)  {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        );
+                        ambilData($('#page').val());
+                        $('#hapusData' + id_kategori).modal('hide');
+                    },
+                    error: function(xhr, thrownError) {
+                        alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                    }
+                });
             }
         });
+    }
+
+    function bukaModalTambah() {
+        $('#tambahData').modal('show');
+        $('.invalid-feedback').empty();
+        $('.form-group').removeClass('has-error').removeClass('has-feedback');
+        $('.form-group .glyphicon').remove();
     }
 </script>
 <?= $this->endSection()?>
