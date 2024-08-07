d57eba3689d78f3b9fed4457e6c502c5b6f660fb
Fajar <fajarsatria4321@gmail.com>
2024-08-07 13:53:29 +0700

    icon

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,4 +1,4 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">Omah Cokelat Pacitan</a>.</strong> All rights
+    <strong>Copyright &copy; 2024 <a href="https://www.instagram.com/omahcokelatpacitan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Omah Cokelat Pacitan</a>.</strong> All rights
     reserved.
 </footer>
